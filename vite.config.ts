516c290b535d8f056a245ef7c9b6ff17ab421f8a
Landon Cassman <Landon.Cassman@gmail.com>
2022-10-18 21:16:38 -0400

    Working react template

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import checker from 'vite-plugin-checker'
-import { EsLinter, linterPlugin, TypeScriptLinter } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
-  plugins: [
-    reactRefresh(),
-    checker({ typescript: true }),
-    linterPlugin({
-      // disabledForBuild: true,
-      include: ['./src/**/*.ts', './src/**/*.tsx'],
-      linters: [
-        new EsLinter({
-          configEnv: configEnv,
-          serveOptions: { cache: false, formatter: 'visualstudio' },
-        }),
-        new TypeScriptLinter(),
-      ],
-    }),
-  ],
+  plugins: [reactRefresh()],
 }))
